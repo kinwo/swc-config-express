2c1dacc5402433bda9e78d83374a24dbcb05d4ba
Henry Chan <henry@kinwo.net>
2023-02-03 11:57:20 +1000

    Project Config
    * add nvm
    * add yarn

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# swc-config
-Node · TypeScript · SWC
+# SWC NodeJS ExpressJS Web Services
+
+NodeJS + TypeScript + SWC + ExpressJS
+
+Ref: https://dev.to/franciscomendes10866/how-to-setup-a-node-api-with-swc-and-eslint-1h5d
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
-import { hello } from "@functions/fn";
+import { hello } from '@functions/fn';
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response): Response => {
-  hello("this is hot reloading and compiling");
-  return res.send("hello world");
+app.get('/', (req: Request, res: Response): Response => {
+  hello('this is hot reloading and compiling');
+  return res.send('Hello Henry');
 });
 
 app.listen(3000);
